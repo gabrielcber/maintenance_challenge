7679fa1dffbbf2bd94ba464eb3ab809af7d8527b
Gabriel Bernardo <gabrielcber@gmail.com>
2022-04-18 10:07:22 -0300

    adding last pipeline to pipeline_registry

--- src/defy/pipeline_registry.py
+++ src/defy/pipeline_registry.py
@@ -5,6 +5,7 @@ from kedro.pipeline import Pipeline, pipeline
 
 from defy.pipelines import data_processing as dp
 from defy.pipelines import data_transforming as dt
+from defy.pipelines import fitting_model as fm
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -15,13 +16,16 @@ def register_pipelines() -> Dict[str, Pipeline]:
 
     data_processing = dp.create_pipeline()
     data_transforming = dt.create_pipeline()
+    fitting_model = fm.create_pipeline()
 
     return {
         "dp": data_processing,
         "dt": data_transforming,
+        "fm": fitting_model,
         "__default__": (
             data_processing
             + data_transforming
+            + fitting_model
             #+ model_metrics_pipeline
             #+ indicium_predictor_pipeline
         ),
